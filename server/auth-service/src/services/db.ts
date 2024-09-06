63080a720a09557b1f2ae803aada5596f00fda4f
Harsh Morayya <harshmorayya@ymail.com>
2024-09-06 14:36:16 +0530

    Added: Kysely setup for auth service

--- server/auth-service/src/services/db.ts
+++ server/auth-service/src/services/db.ts
@@ -1,22 +1,37 @@
-import mongoose from 'mongoose'
-import logger from '../common/logger'
+import logger from "../common/logger"
+import { DB } from "kysely-codegen"
+import { Pool } from "pg"
 
 // to use env variables
-import '../common/env'
+import "../common/env"
+import { Kysely, PostgresDialect } from "kysely"
 
-const DB_URI = process.env.MONGO_URI || process.env.LOCAL_CONNECTION_STRING
-
-mongoose.connect(DB_URI)
-
-mongoose.Promise = global.Promise
+const DB_URI = process.env.DATABASE_URL
 
 // Get current connected Database
-const db = mongoose.connection
+const db = new Kysely<DB>({
+  dialect: new PostgresDialect({
+    pool: new Pool({
+      connectionString: DB_URI,
+    }),
+  }),
+})
 
 // Notify on error or success
-db.on('error', (err) => logger.error('connection with db error', err))
-db.on('close', () => logger.info('connection closed to db'))
-db.once('open', () => logger.info(`Connected to the database instance on ${DB_URI}`))
+// db.on("error", (err) => logger.error("connection with db error", err))
+// db.on("close", () => logger.info("connection closed to db"))
+// db.once("open", () => logger.info(`Connected to the database instance on ${DB_URI}`))
+
+async function checkDBConn() {
+  try {
+    await db.selectFrom("roles").execute()
+    logger.info(`Connection to the database on ${DB_URI}`)
+  } catch (err) {
+    logger.error("Connection with db error: " + err)
+  }
+}
+
+checkDBConn()
 
 export default {
   Connection: db,
